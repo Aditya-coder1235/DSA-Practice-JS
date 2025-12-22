c37cfc5e3e6a9b4e01b2ccb43d0f821aceaa699e
Aditya Girawale <adityagirawale2003@gmail.com>
2025-12-22 07:34:41 +0530

    hash-map

---
+++ Hashing/app.js
@@ -0,0 +1,60 @@
+class HashMap{
+    constructor(size=7){
+        this.dataMap=new Array(size)
+    }
+
+    #hash(key){
+        let hash=0;
+        for(let i=0; i<key.length; i++){
+            hash=(hash+key.charCodeAt(i)*23)%this.dataMap.length
+        }
+        return hash
+    }
+
+    set(key,value){
+        let index=this.#hash(key)
+        if(!this.dataMap[index]){
+            this.dataMap[index]=[]
+        }
+        for(let i=0; i<this.dataMap[index].length; i++){
+            if(this.dataMap[index][i][0]===key){
+                this.dataMap[index][i][1]=value
+                return true
+            }
+        }
+
+        this.dataMap[index].push([key,value])
+        return true
+    }
+
+    get(key){
+        let index=this.#hash(key)
+        if(this.dataMap[index]){
+            for(let i=0; i<this.dataMap[index].length; i++){
+                if(this.dataMap[index][i][0]===key){
+                    return this.dataMap[index][i][1]
+                }
+            }
+        }
+        return undefined
+    }
+
+    keys(){
+        let allKeys=[]
+        for(let i=0; i<this.dataMap.length; i++){
+            if(this.dataMap[i]){
+                for(let j=0; j<this.dataMap[i].length; j++){
+                    allKeys.push(this.dataMap[i][j][0])
+                }
+            }
+        }
+        return allKeys
+    }
+}
+
+
+const myHash=new HashMap()
+myHash.set('A','Aditya')
+myHash.set('K', 'Kailas')
+myHash.set('G', 'Girawale')
+console.log(myHash)
